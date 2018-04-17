549dca9d7ae5bd5436613e282b112843b0f35431
Michał Simoniuksztis <m.simoniuksztis@i4b.pl>
2018-04-17 19:27:46 +0200

    first commit

---
+++ EFModel using the Entity Data Model Wizard/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFModel_using_the_Entity_Data_Model_Wizard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
